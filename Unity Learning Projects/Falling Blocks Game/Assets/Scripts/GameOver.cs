2d2e77106790b14e55a7539ea529e0231df89ee2
sgarg03 <sat.garg03@gmail.com>
2020-11-17 19:23:51 -0800

    Fixed screen rotate bug and added highscore UI

--- Unity Learning Projects/Falling Blocks Game/Assets/Scripts/GameOver.cs
+++ Unity Learning Projects/Falling Blocks Game/Assets/Scripts/GameOver.cs
@@ -7,11 +7,16 @@ using UnityEngine.SceneManagement;
 public class GameOver : MonoBehaviour {
     public GameObject gameOverScreen;
     public GameObject timeKeeper;
+    float highscore = 0;
     public Text secondsSurvivedUI;
+    public Text highscoreUI;
     bool gameOver;
 
     void Start() {
         FindObjectOfType<PlayerControl>().OnPlayerDeath += OnGameOver;
+
+        highscore = PlayerPrefs.GetFloat("highscore");
+        highscoreUI.text = highscore.ToString();
     }
     // Update is called once per frame
     void Update() {
@@ -29,6 +34,11 @@ public class GameOver : MonoBehaviour {
         gameOverScreen.SetActive(true);
         timeKeeper.SetActive(false);
         secondsSurvivedUI.text = Time.timeSinceLevelLoad.ToString("F1");
+        if (Time.timeSinceLevelLoad > highscore) {
+            highscore = Time.timeSinceLevelLoad;
+            PlayerPrefs.SetFloat("highscore", highscore);
+        }
+        highscoreUI.text = highscore.ToString("F1");
         gameOver = true;
     }
 }
